cdde20c9e3a2a0fdbe1195d7e3273d75e6e7eca4
amirmub <amirmubarek01@gmail.com>
2025-01-10 20:20:52 +0300

    i have added js code rotate text js code and switch between about buttons laso added portfolio fillter

--- script.js
+++ script.js
@@ -13,3 +13,40 @@ navlist.addEventListener("click",()=>{
     menuIcon.classList.remove("active");
     document.body.classList.remove("open");
 });
+
+// rotate text js code 
+let text = document.querySelector(".text p");
+
+text.innerHTML = text.innerHTML.split("").map((char,i)=>
+    `<b style="transform:rotate(${i * 6.3}deg")>${char}</b>`
+).join("");
+
+
+// switch between about buttons 
+
+const buttons = document.querySelectorAll('.about-btn button');
+const contents = document.querySelectorAll('.content');
+
+buttons.forEach((button, index) => {
+  button.addEventListener('click', function(e) {
+    e.preventDefault();
+    contents.forEach(content => content.style.display = 'none');
+    contents[index].style.display = 'block';
+    buttons.forEach(btn => btn.classList.remove('active'));
+    button.classList.add('active');
+  });
+});
+
+
+
+// portfolio fillter 
+
+var mixer = mixitup('.portfolio-gallery',{
+    selectors: {
+        target: '.portfolio-box'
+    },
+    animation: {
+        duration: 500
+    }
+});
+
